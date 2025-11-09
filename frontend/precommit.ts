4f32a78e9674c512ad8aaa74884b431837027e8b
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2025-11-09 19:47:47 +0100

    add precommit bun task

---
+++ frontend/precommit.ts
@@ -0,0 +1,23 @@
+// import { $ } from "bun"
+
+import type { SpawnOptions } from 'bun';
+
+const spawnOptions: SpawnOptions.OptionsObject = {
+	stdin: 'inherit',
+	stdout: 'inherit',
+	stderr: 'inherit'
+};
+
+const run = async () => {
+	Bun.spawn(['bun', 'run', 'format'], spawnOptions);
+	Bun.spawn(['bun', 'run', 'lint'], spawnOptions);
+	Bun.spawn(['bun', 'run', 'test'], spawnOptions);
+
+	// process.on('SIGINT', async () => {
+	// 	console.log('Cleaning up...');
+	// 	// Bun.spawn(["bun", "run", "db:down"])
+	// 	// await $`bun run db:down` will also work
+	// });
+};
+
+run();
